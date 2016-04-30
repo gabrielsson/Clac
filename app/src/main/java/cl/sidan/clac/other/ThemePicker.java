9216f1ff6fa22e8341141490be245edaf1bd7a89
Max Gabrielsson <max.gabrielsson@gmail.com>
2016-04-30 22:05:37 +0200

    New classic theme

--- app/src/main/java/cl/sidan/clac/other/ThemePicker.java
+++ app/src/main/java/cl/sidan/clac/other/ThemePicker.java
@@ -6,21 +6,22 @@ import java.util.List;
 import cl.sidan.clac.R;
 
 public class ThemePicker {
-    static Integer[] themes = {R.style.DefaultTheme_NoActionBar, R.style.NightTheme_NoActionBar};
-    static String[] names = {"Default", "Night"};
+    //Set the NoActionBar since we are not using the actionbar, otherwise we will get crashes (npe)
+    static Integer[] themes = {R.style.DefaultTheme_NoActionBar, R.style.NightTheme_NoActionBar, R.style.ClassicTheme_NoActionBar};
+    static String[] names = {"Default", "Night", "Classic"};
 
     static final List<Integer> themeStyles = Arrays.asList(themes);
     static final List<String> themeNames = Arrays.asList(names);
 
     public static Integer getTheme(int position) {
-        if (0 > position || position >= themeStyles.size()) {
+        if (0 > position || position > themeStyles.size()) {
             position = 0;
         }
         return themeStyles.get(position);
     }
 
     public static String getThemeName(int position) {
-        if (0 > position || position >= themeNames.size()) {
+        if (0 > position || position > themeNames.size()) {
             position = 0;
         }
         return themeNames.get(position);

f1e46f83355cfeafda790e456322f5eb4ddb12cc
Sebastian Weddmark Olsson <visnae@gmail.com>
2016-05-04 14:32:24 +0200

    Removed unused imports

--- app/src/main/java/cl/sidan/clac/fragments/FragmentSearch.java
+++ app/src/main/java/cl/sidan/clac/fragments/FragmentSearch.java
@@ -1,9 +1,7 @@
 package cl.sidan.clac.fragments;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +10,6 @@ import android.widget.EditText;
 
 import cl.sidan.clac.MainActivity;
 import cl.sidan.clac.R;
-import cl.sidan.clac.fragments.FragmentReadEntries;
 
 public class FragmentSearch extends Fragment {
     private View rootView;

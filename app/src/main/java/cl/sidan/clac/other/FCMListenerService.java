53a71b2a7d17113c7716bed9c8238eae364542d4
Sebastian Weddmark Olsson <visnae@gmail.com>
2016-08-28 19:11:11 +0200

    Update Notifications

--- app/src/main/java/cl/sidan/clac/other/FCMListenerService.java
+++ app/src/main/java/cl/sidan/clac/other/FCMListenerService.java
@@ -1,12 +1,10 @@
 package cl.sidan.clac.other;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -30,7 +28,7 @@ public class FCMListenerService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         String fromID = remoteMessage.getFrom(),
-                body = "";
+                body;
 
         Map data = remoteMessage.getData();
 
@@ -85,20 +83,42 @@ public class FCMListenerService extends FirebaseMessagingService {
         PendingIntent pendingIntent = PendingIntent.getActivity(
                 this, 0 /* Request code */, intent, PendingIntent.FLAG_ONE_SHOT);
 
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        /*
+         * Something like this would add a reply button below the notification.
+         *
 
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+        // Key for the string that's delivered in the action's intent.
+        String KEY_TEXT_REPLY = "key_text_reply";
+        String replyLabel = "Reply";
+        RemoteInput remoteInput = new RemoteInput.Builder(KEY_TEXT_REPLY)
+                .setLabel(replyLabel)
+                .build();
+
+        // Create the reply action and add the remote input.
+        Notification.Action replyAction = new Notification.Action.Builder(
+                R.drawable.smiley6, "Gilla", pendingIntent)
+                .addRemoteInput(remoteInput)
+                .build();
+         */
+
+        Notification.Builder notificationBuilder = new Notification.Builder(this)
                 // .setColor(R.color.colorPrimaryPink)
                 .setSmallIcon(R.drawable.nopo)
                 .setContentTitle("Message from " + user)
                 .setContentText(message)
+                .setStyle(new Notification.BigTextStyle().bigText(message)) // Multiline
+                // .addAction(replyAction)
                 .setAutoCancel(true)
-                .setSound(defaultSoundUri)
+                .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE)
+                .setPriority(Notification.PRIORITY_DEFAULT)
+                .setLights(0xff00ff00, 300, 500)
                 .setContentIntent(pendingIntent);
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        notificationManager.notify(
+                (int) System.currentTimeMillis() /* (unique) ID of notification */,
+                notificationBuilder.build());
     }
 }
